469fc35b4a59fcbe233bebd7cc99b83df91c9f15
João Antunes <joaofbantunes@gmail.com>
2019-02-09 10:54:01 +0000

    Adding project to source control

---
+++ src/AspNetCoreRazorPagesInnerModelLocalizationSample.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreRazorPagesInnerModelLocalizationSample.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Display(Name = "Test")]
+            public string Test { get; set; }
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ src/AspNetCoreRazorPagesInnerModelLocalizationSample.Web/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+
+namespace AspNetCoreRazorPagesInnerModelLocalizationSample.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+                .AddDataAnnotationsLocalization();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var supportedCultures = new[]
+             {
+                new CultureInfo("en"),
+                new CultureInfo("pt"),
+            };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                // Formatting numbers, dates, etc.
+                SupportedCultures = supportedCultures,
+                // UI strings that we have localized.
+                SupportedUICultures = supportedCultures
+            });
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
